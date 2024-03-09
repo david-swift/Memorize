f06426ee311ea587c6672f9dabfb5c0579eb405c
david-swift <haenerdavid@icloud.com>
2024-03-09 22:36:56 +0100

    Set app version

--- Sources/Flashcards.swift
+++ Sources/Flashcards.swift
@@ -21,7 +21,7 @@ struct Flashcards: App {
         .title("Memorize")
         .quitShortcut()
         .overlay {
-            AboutWindow(id: "about", appName: "Memorize", developer: "david-swift", version: "0.1.3")
+            AboutWindow(id: "about", appName: "Memorize", developer: "david-swift", version: "0.1.4")
                 .icon(.custom(name: "io.github.david_swift.Flashcards"))
                 .website(.init(string: "https://github.com/david-swift/Memorize"))
                 .issues(.init(string: "https://github.com/david-swift/Memorize/issues"))
