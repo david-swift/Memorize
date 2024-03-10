dfcaa9e15d58712048af480cb1e963d7d049a646
david-swift <haenerdavid@icloud.com>
2024-03-10 10:48:56 +0100

    Update to Localized 0.2.2
    This makes the Brazilian Portuguese translation work

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     name: "Flashcards",
     dependencies: [
         .package(url: "https://github.com/AparokshaUI/Adwaita", from: "0.2.2"),
-        .package(url: "https://github.com/AparokshaUI/Localized", from: "0.2.0")
+        .package(url: "https://github.com/AparokshaUI/Localized", from: "0.2.2")
     ],
     targets: [
         .executableTarget(
