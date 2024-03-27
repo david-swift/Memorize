224820f33b242f6012e36e974422ac70a603c6a5
david-swift <haenerdavid@icloud.com>
2024-03-27 12:05:51 +0100

    Remove sort function of flashcard

--- Sources/Model/Flashcard.swift
+++ Sources/Model/Flashcard.swift
@@ -5,7 +5,7 @@
 
 import Foundation
 
-struct Flashcard: SearchScore, Identifiable, Codable {
+struct Flashcard: Identifiable, Codable {
 
     let id: String
     var front = ""
@@ -45,19 +45,6 @@ struct Flashcard: SearchScore, Identifiable, Codable {
     mutating func done() {
         gameData.input = ""
         gameData.lastInput = nil
-    }
-
-    func score(_ query: String?) -> Int {
-        var totalScore = 1
-        if let query, !query.isEmpty {
-            totalScore = 0
-            totalScore += front.search(query) ? 5 : 0
-            totalScore += back.search(query) ? 5 : 0
-            for tag in tags.nonOptional {
-                totalScore += tag.search(query) ? 1 : 0
-            }
-        }
-        return totalScore
     }
 
 }
