fd3731c46d55bd323256b8ddd60d687bbf9f1ef3
Konstantin Tutsch <mail@konstantintutsch.com>
2024-03-28 17:41:54 +0100

    Add JSON to SQLite conversion

--- Sources/Model/Database.swift
+++ Sources/Model/Database.swift
@@ -16,17 +16,42 @@ class Database {
         }
     }
 
+    // sets table layout
+    let setsTable = Table("sets")
+    let setsId = Expression<Int64>("id")
+    let name = Expression<String>("name")
+    let answerSide = Expression<String>("answerSide")
+
+    // flashcards table layout
+    let flashcardsTable = Table("flashcards")
+    let flashcardsId = Expression<Int64>("id")
+    let setsTableId = Expression<Int64>("sets_id")
+    let difficulty = Expression<Int?>("difficulty")
+    let front = Expression<String?>("front")
+    let back = Expression<String?>("back")
+
     init() {
         sets = []
-        print("Check JSON file, maybe create new one if not available")
-        checkFile()
-        print("Read value from JSON file")
-        readValue()
 
         do {
             let database = try Connection(databasePath().path)
 
+            print("Check data directory")
+            checkDir()
+
             checkDatabase(database)
+            let fileManager = FileManager.default
+            if fileManager.fileExists(atPath: filePath().path) {
+                print("Read value from JSON file")
+                readValue()
+
+                print("Insert JSON to SQLite")
+                insertJSON(database)
+
+                let backupPath = filePath().path + ".bak"
+                try fileManager.moveItem(atPath: filePath().path, toPath: backupPath)
+                print("Conversion finished. Backup of JSON file at " + backupPath)
+            }
         } catch {
             print(error)
         }
@@ -52,8 +77,8 @@ class Database {
         dirPath().appendingPathComponent("data.sqlite3")
     }
 
-    /// Check whether the settings file exists, and, if not, create it.
-    private func checkFile() {
+    /// Check whether the data directory exists, and, if not, create it.
+    private func checkDir() {
         let fileManager = FileManager.default
         if !fileManager.fileExists(atPath: dirPath().path) {
             try? fileManager.createDirectory(
@@ -62,41 +87,50 @@ class Database {
                 attributes: nil
             )
         }
-        if !fileManager.fileExists(atPath: filePath().path) {
-            fileManager.createFile(atPath: filePath().path, contents: .init(), attributes: nil)
-        }
     }
 
-    // Check whether the database file exists, and, if not, initialize it.
+    // Check whether the database layout exists, and, if not, create it.
     private func checkDatabase(_ database: Connection) {
         do {
-            let setsTable = Table("sets")
-            let flashcardsTable = Table("flashcards")
-
-            // sets table layout
-            let setsId = Expression<Int64>("id")
-            let setsName = Expression<String>("name")
-            let answerSide = Expression<String>("answerSide")
-
+            // sets table rules
             try database.run(setsTable.create(ifNotExists: true) { table in
                 table.column(setsId, primaryKey: .autoincrement)
-                table.column(setsName)
+                table.column(name)
                 table.column(answerSide)
                 table.check(answerSide == "front" || answerSide == "back")
             })
 
-            // flashcards table layout
-            let flashcardsId = Expression<Int64>("id")
-            let difficulty = Expression<Int64>("difficulty")
-            let front = Expression<String?>("front")
-            let back = Expression<String?>("back")
-
+            // flashcards table rules
             try database.run(flashcardsTable.create(ifNotExists: true) { table in
                 table.column(flashcardsId, primaryKey: .autoincrement)
-                table.column(difficulty)
+                table.column(setsTableId, references: setsTable, setsId)
+                table.column(difficulty, defaultValue: 0)
                 table.column(front)
                 table.column(back)
             })
+        } catch {
+            print(error)
+        }
+    }
+
+    // Parse the JSON file (old save format) to the database
+    private func insertJSON(_ database: Connection) {
+        do {
+            for (index, item) in sets.enumerated() {
+                try database.run(setsTable.insert(
+                    name <- item.name,
+                    answerSide <- item.answerSide.rawValue
+                ))
+
+                for flashcard in item.flashcards {
+                    try database.run(flashcardsTable.insert(
+                        setsTableId <- Int64(index + 1),
+                        difficulty <- flashcard.gameData.difficulty,
+                        front <- flashcard.front,
+                        back <- flashcard.back
+                    ))
+                }
+            }
         } catch {
             print(error)
         }
