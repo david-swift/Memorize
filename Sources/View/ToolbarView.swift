7593a76a27a79692ebfea44bb72be325d115a1a0
david-swift <haenerdavid@icloud.com>
2024-05-20 12:55:10 +0200

    Prepare for release 0.2.3

--- Sources/View/ToolbarView.swift
+++ Sources/View/ToolbarView.swift
@@ -36,7 +36,7 @@ struct ToolbarView: View {
             visible: $about,
             app: "Memorize",
             developer: "david-swift",
-            version: "0.2.2",
+            version: "0.2.3",
             icon: .custom(name: "io.github.david_swift.Flashcards"),
             website: .init(string: "https://github.com/david-swift/Memorize"),
             issues: .init(string: "https://github.com/david-swift/Memorize/issues")
